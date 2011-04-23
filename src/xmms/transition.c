04230c12d056b9c1df32f26aa0d4ff1399892ac3
David Richards <rawdod@gmail.com>
2011-04-22 21:34:57 -0400

    enable configing of single source transitions

--- src/xmms/transition.c
+++ src/xmms/transition.c
@@ -35,14 +35,6 @@ struct xmms_transition_St {
 };
 
 struct xmms_transitions_St {
-
-	xmms_transition_t *starting;
-	xmms_transition_t *stopping;
-	xmms_transition_t *pausing;
-	xmms_transition_t *resuming;
-	xmms_transition_t *seeking;
-	xmms_transition_t *jumping;
-	xmms_transition_t *advancing;
 	
 	xmms_transition_t *transitions[7];
 
@@ -82,12 +74,24 @@ xmms_transitions_destroy (xmms_transitions_t *transitions)
 
 	g_return_if_fail (transitions);
 
+	gint t;
 
-	xmms_transition_destroy(transitions->starting);
-
+	for(t = 0; t < 7; t++) {
+		xmms_transition_destroy(transitions->transitions[t]);
+	}
 	g_free (transitions);
 
 	XMMS_DBG ("Destroying transitions");
+
+}
+
+static void
+update_transitions_config (xmms_object_t *object, xmmsv_t *data,
+                          gpointer userdata)
+{
+
+
+	XMMS_DBG ("Update transitions config called");
 
 }
 
@@ -97,24 +101,73 @@ xmms_transitions_new ()
 
 	xmms_transitions_t *transitions = g_new0 (xmms_transitions_t, 1);
 
+	const char *transition_strings[] = { "starting", "stopping", "pausing", "resuming", "seeking", "jumping", "advancing" };
+
 	XMMS_DBG ("Setting up transitions");
-	
-	// read all the config values and load up all the plugins into the structs
 
 	xmms_transition_plugin_t *plugin;
 	xmms_config_property_t *cv;
-	const gchar *name = "fade";
 	
-		cv = xmms_config_property_register ("transition.plugin",
-	                                    "fade",
-	                                    NULL, NULL);
+	int t;
+	gint stack_no; // well call the 'chain' of plugins a 'stack' since we use that chain word elsewhere
+	xmms_config_property_t *cfg;
+	gchar key[64];
+	const gchar *name;
+		
+
+	for(t = 0; t < 7; t++) {
 	
-	name = xmms_config_property_get_string (cv);
-	xmms_log_info ("Using transition plugin: %s", name);
-	plugin = (xmms_transition_plugin_t *)xmms_plugin_find (XMMS_PLUGIN_TYPE_TRANSITION, name);
+		for (stack_no = 0; TRUE; stack_no++) {
 
-	transitions->starting = xmms_transition_new(plugin);
+			g_snprintf (key, sizeof (key), "transition.%s.%i", transition_strings[t], stack_no);
+		
+			if ((!stack_no)) {
+				xmms_config_property_register (key, "", update_transitions_config,
+			                               NULL);
+			}
 
+			cfg = xmms_config_lookup (key);
+			
+			// Single Source starting/stopping/pausing/resuming
+			if (t < 4) { 
+
+				if (!cfg) {
+					// no config exists
+					XMMS_DBG ("No config for %s transition plugin number %d in stack", transition_strings[t], stack_no);
+					transitions->transitions[t] = xmms_transition_new(NULL);
+					break;
+				}
+			
+				name = xmms_config_property_get_string (cfg);
+
+				if (!name[0]) {
+					// config exists, but is empty
+					XMMS_DBG ("No value for %s transition plugin number %d in stack", transition_strings[t],stack_no);
+					transitions->transitions[t] = xmms_transition_new(NULL);
+					break;
+				}
+
+				// config exists and is something
+				XMMS_DBG ("Trying %s plugin for %s transition as effect number %d in stack", name, transition_strings[t], stack_no);
+				
+				plugin = (xmms_transition_plugin_t *)xmms_plugin_find (XMMS_PLUGIN_TYPE_TRANSITION, name);
+
+				if (plugin == NULL) {
+					XMMS_DBG ("Cant find plugin called %s", name);
+					transitions->transitions[t] = xmms_transition_new(NULL);
+					break;
+				} else {
+					transitions->transitions[t] = xmms_transition_new(plugin);
+					XMMS_DBG ("%s plugin set for %s transition as effect %d in stack", name, transition_strings[t], stack_no);
+				}
+		
+			} else {
+				// Dual Source Seeking, Jumping Advancing
+				XMMS_DBG ("Skipping dual source %s transition", transition_strings[t]);
+				break;
+			}
+		}
+	}
 
 	XMMS_DBG ("Transitions setup complete");
 	
@@ -130,8 +183,12 @@ xmms_transition_new (xmms_transition_plugin_t *plugin)
 
 	xmms_transition_t *transition = g_new0 (xmms_transition_t, 1);
 
-	return xmms_transition_add (plugin, transition);
-
+	if (plugin != NULL) {
+		return xmms_transition_add (plugin, transition);
+	} else {
+		transition->enabled = false;
+		return transition;
+	}
 }
  
 static xmms_transition_t *
