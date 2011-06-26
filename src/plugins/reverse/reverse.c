7c3743f50ac4881765631830cb8796e74b7379ba
David Richards <rawdod@gmail.com>
2011-06-26 02:14:46 -0400

    dont make things play in reverse by default

--- src/plugins/reverse/reverse.c
+++ src/plugins/reverse/reverse.c
@@ -74,7 +74,7 @@ xmms_reverse_plugin_setup (xmms_xform_plugin_t *xform_plugin)
 
 	xmms_xform_plugin_methods_set (xform_plugin, &methods);
 
-	xmms_xform_plugin_config_property_register (xform_plugin, "enabled", "1",
+	xmms_xform_plugin_config_property_register (xform_plugin, "enabled", "0",
 	                                            NULL, NULL);
 
 	xmms_xform_plugin_indata_add (xform_plugin,
